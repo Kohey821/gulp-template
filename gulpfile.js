cf64fb6e139d17f4f4c5444ec1404817389e2b11
kohey <very.tasty.konjac@gmail.com>
2021-09-09 00:20:30 +0900

    インデント変更と不必要箇所削除。環境によってはfiberがエラーになるので一旦コメントアウト

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const uglify = require('gulp-uglify');
 const pipline = require('readable-stream').pipeline;
 
 const sass = require('gulp-sass')(require('sass'));
-const Fiber = require('fibers');;
+// const Fiber = require('fibers');;
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 
@@ -18,141 +18,132 @@ const svgmin = require('gulp-svgmin');
 
 const compileConfig = require('./gulpfile-config-compile');
 
-let browserSyncConfig = null;
-try {
-    browserSyncConfig = require('./gulpfile-config-browser-sync');
-} catch {
-    console.log(`
-        --------------------------------------
-        gulpfile-config-browser-sync.js
-        を作成すると幸せになれます。
-        --------------------------------------
-    `);
-}
+let browserSyncConfig = require('./gulpfile-config-browser-sync');
 
 const checkNested = (obj, level, ...rest) => {
-    if (typeof obj === 'undefined') return false;
-    if (rest.length == 0 && obj.hasOwnProperty(level)) return true;
-    return checkNested(obj[level], ...rest);
+  if (typeof obj === 'undefined') return false;
+  if (rest.length == 0 && obj.hasOwnProperty(level)) return true;
+  return checkNested(obj[level], ...rest);
 };
 
 const configExist = (...prop) => {
-    return checkNested(...prop);
+  return checkNested(...prop);
 };
 
 const streamCss = (stream) => {
-    return stream
-        .pipe(browserSync.stream());
+  return stream
+    .pipe(browserSync.stream());
 };
 
 const compileJs = (cb) => {
-    if (configExist(compileConfig, 'js')) {
-        for (let i of compileConfig.js) {
-            pipline(
-                src(i.src),
-                sourcemaps.init(),
-                concat(i.name),
-                babel({
-                    presets: ['@babel/env']
-                }),
-                uglify({
-                    keep_fnames: true,
-                    mangle: false,
-                }),
-                sourcemaps.write('.'),
-                dest(i.dest),
-                dest(i.dest)
-            );
-        }
+  if (configExist(compileConfig, 'js')) {
+    for (let i of compileConfig.js) {
+      pipline(
+        src(i.src),
+        sourcemaps.init(),
+        concat(i.name),
+        babel({
+          presets: ['@babel/env']
+        }),
+        uglify({
+          keep_fnames: true,
+          mangle: false,
+        }),
+        sourcemaps.write('.'),
+        dest(i.dest),
+        dest(i.dest)
+      );
     }
+  }
 
-    cb();
+  cb();
 };
 
 const compileSass = (cb) => {
-    if (configExist(compileConfig, 'sass')) {
-        for (let i of compileConfig.sass) {
-            src(i.src)
-                .pipe(sourcemaps.init())
-                .pipe(
-                    sass({
-                        outputStyle: "compressed",
-                        fiber: Fiber,
-                    })
-                        .on('error', sass.logError))
-                .pipe(postcss([ autoprefixer() ]))
-                .pipe(sourcemaps.write('.'))
-                .pipe(dest(i.dest))
-                .pipe(dest(i.dest));
-        }
+  if (configExist(compileConfig, 'sass')) {
+    for (let i of compileConfig.sass) {
+      src(i.src)
+        .pipe(sourcemaps.init())
+        .pipe(
+          sass({
+            outputStyle: "compressed",
+            // fiber: Fiber,
+          })
+            .on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(i.dest))
+        .pipe(dest(i.dest));
     }
+  }
 
-    cb();
+  cb();
 };
 
 const compileSvg = (cb) => {
-    if (configExist(compileConfig, 'svg')) {
-        for (let i of compileConfig.svg) {
-            src(i.src)
-                .pipe(svgmin({
-                    plugins: [
-                        {cleanupIDs: false},
-                    ],
-                }))
-                .pipe(dest(i.dest))
-                .pipe(dest(i.dest));
-        }
+  if (configExist(compileConfig, 'svg')) {
+    for (let i of compileConfig.svg) {
+      src(i.src)
+        .pipe(svgmin({
+          plugins: [
+            {cleanupIDs: false},
+          ],
+        }))
+        .pipe(dest(i.dest))
+        .pipe(dest(i.dest));
     }
+  }
 
-    cb();
+  cb();
 };
 
 const setBrowserSyncEvents = (cb) => {
-    if (browserSyncConfig) {
-        browserSync.init(browserSyncConfig.init);
+  if (browserSyncConfig) {
+    browserSync.init(browserSyncConfig.init);
 
-        watch(browserSyncConfig.stream, () => {
-            return streamCss(src(browserSyncConfig.stream));
-        });
+    watch(browserSyncConfig.stream, () => {
+      return streamCss(src(browserSyncConfig.stream));
+    });
 
-        watch(browserSyncConfig.reload, (cb) => {
-            browserSync.reload();
+    watch(browserSyncConfig.reload, (cb) => {
+      browserSync.reload();
 
-            cb();
-        });
-    }
+      cb();
+    });
+  }
 
-    cb();
+  cb();
 };
 
 const dev = (cb) => {
-    if (configExist(compileConfig, 'js')) {
-        for (let i of compileConfig.js) {
-            watch(i.src, compileJs);
-        }
+  if (configExist(compileConfig, 'js')) {
+    for (let i of compileConfig.js) {
+      watch(i.src, compileJs);
     }
+  }
 
-    if (configExist(compileConfig, 'sass')) {
-        for (let i of compileConfig.sass) {
-            watch(i.src, compileSass);
-        }
+  if (configExist(compileConfig, 'sass')) {
+    for (let i of compileConfig.sass) {
+      watch(i.src, compileSass);
     }
+  }
 
-    if (configExist(compileConfig, 'svg')) {
-        for (let i of compileConfig.svg) {
-            watch(i.src, compileSvg);
-        }
+  if (configExist(compileConfig, 'svg')) {
+    for (let i of compileConfig.svg) {
+      watch(i.src, compileSvg);
     }
+  }
 
-    cb();
+  cb();
 };
 
 exports.default = series(
-    parallel(
-        compileJs,
-        compileSass,
-        compileSvg
-    ),
-    setBrowserSyncEvents,
-    dev
+  parallel(
+    compileJs,
+    compileSass,
+    compileSvg
+  ),
+  setBrowserSyncEvents,
+  dev
 );
+
